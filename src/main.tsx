5e6ddb06f7f15f9a3c80703646f8cf58dc28171f
Joohyung Park <dochipjh@gmail.com>
2025-12-06 03:36:45 +0900

    feat: 인터뷰 데이터 유지 현상 해결

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 import App from "./App.tsx"
+import { useInterviewStore } from "./stores/interviewStore" // Import useInterviewStore
+
+// Reset the Zustand store state when the application loads
+useInterviewStore.getState().reset();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
